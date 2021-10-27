cab4302a27e008684cb5c8aacce74d95bad8a740
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-10-27 21:22:25 +0530

    Solve concatenation-of-array

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - [Valid Mountain Array](https://leetcode.com/problems/valid-mountain-array/)
 - [1920. Build Array from Permutation](https://leetcode.com/problems/build-array-from-permutation/)
+- [Concatenation of Array](https://leetcode.com/problems/concatenation-of-array/)
---
+++ src/concatenation_of_array/Solution.java
@@ -0,0 +1,14 @@
+package concatenation_of_array;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] arr = new int[nums.length * 2];
+        
+        for (int i = 0; i < nums.length; ++i) {
+            arr[i] = nums[i];
+            arr[i + nums.length] = nums[i];
+        }
+        
+        return arr;
+    }
+}

ae3d44261e356718fe46c7171b889b3c4bc9d14d
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-12-05 16:15:44 +0530

    Solve house_robber_iii

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 - [1920. Build Array from Permutation](https://leetcode.com/problems/build-array-from-permutation/)
 - [Concatenation of Array](https://leetcode.com/problems/concatenation-of-array/)
 - [Fibonacci Number](https://leetcode.com/problems/fibonacci-number/)
-- [N-th Tribonacci Number](https://leetcode.com/problems/n_th_tribonacci_number/)
+- [N-th Tribonacci Number](https://leetcode.com/problems/nth_tribonacci-number/)
+- [House Robber III](https://leetcode.com/problems/house-robber-iii/)
---
+++ src/house_robber_iii/Solution.java
@@ -0,0 +1,52 @@
+package house_robber_iii;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    private final Map<TreeNode, Integer> map = new HashMap<>();
+
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (map.containsKey(root)) {
+            return map.get(root);
+        }
+
+        int sum = root.val;
+
+        if (root.left != null) {
+            sum += rob(root.left.left);
+            sum += rob(root.left.right);
+        }
+
+        if (root.right != null) {
+            sum += rob(root.right.left);
+            sum += rob(root.right.right);
+        }
+
+        map.put(root, Integer.max(sum, (rob(root.left) + rob(root.right))));
+        return map.get(root);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

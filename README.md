241da1c82606ecc47df888deabb78117269e7bed
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-12-05 12:40:26 +0530

    Solve fibonacci-number

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [Valid Mountain Array](https://leetcode.com/problems/valid-mountain-array/)
 - [1920. Build Array from Permutation](https://leetcode.com/problems/build-array-from-permutation/)
 - [Concatenation of Array](https://leetcode.com/problems/concatenation-of-array/)
+- [Fibonacci Number](https://leetcode.com/problems/fibonacci-number/)
---
+++ src/fibonacci_number/Solution.java
@@ -0,0 +1,27 @@
+package fibonacci_number;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().fib(30));
+    }
+
+    public int fibonacci(int[] arr, int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        if (arr[n] == -1) {
+            arr[n] = fibonacci(arr, n - 1) + fibonacci(arr, n - 2);
+        }
+
+        return arr[n];
+    }
+
+    public int fib(int n) {
+        int[] arr = new int[n + 1];
+        Arrays.fill(arr, -1);
+        return fibonacci(arr, n);
+    }
+}

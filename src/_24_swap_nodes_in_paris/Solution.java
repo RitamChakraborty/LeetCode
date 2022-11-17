846862d4344d607e2def9fd10ed1bc2b10b13d1b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-11-17 22:47:32 +0530

    Solve 24

---
+++ src/_24_swap_nodes_in_paris/Solution.java
@@ -0,0 +1,34 @@
+package _24_swap_nodes_in_paris;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode node = head;
+
+        while (node != null && node.next != null) {
+            int temp = node.val;
+            node.val = node.next.val;
+            node.next.val = temp;
+            node = node.next.next;
+        }
+
+        return head;
+    }
+}

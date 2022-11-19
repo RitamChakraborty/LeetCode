42053db013c1fc3e9ab9b6de5d0e719687f65c6b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-11-19 21:52:24 +0530

    Solve 203

---
+++ src/_203_remove_linked_list_elements/Solution.java
@@ -0,0 +1,34 @@
+package _203_remove_linked_list_elements;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode fakeHead = new ListNode();
+        fakeHead.next = head;
+        ListNode node = fakeHead;
+
+        while (node.next != null) {
+            if (node.next.val == val) node.next = node.next.next;
+            else node = node.next;
+        }
+
+        return fakeHead.next;
+    }
+}
+
+class Solution1 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}

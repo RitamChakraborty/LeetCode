7dd636deebe7740e1b2761cb9c192a04d735af19
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-08-05 15:58:03 +0530

    Solve 75

--- src/_75_sort_colors/Solution.java
+++ src/_75_sort_colors/Solution.java
@@ -1,26 +1,51 @@
 package _75_sort_colors;
 
-class Solution {
-    public void sortColors(int[] arr) {
-        int low = 0;
-        int mid = 0;
-        int high = arr.length - 1;
+import java.util.Arrays;
 
-        while (mid <= high) {
-            if (arr[mid] == 1) {
-                ++mid;
-            } else if (arr[mid] == 0) {
-                int temp = arr[low];
-                arr[low] = arr[mid];
-                arr[mid] = temp;
-                ++mid;
-                ++low;
-            } else if (arr[mid] == 2) {
-                int temp = arr[high];
-                arr[high] = arr[mid];
-                arr[mid] = temp;
-                --high;
+class Solution {
+    public void sortColorsBruteForce(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    public void sortColorsBetter(int[] arr) {
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+
+        for (int num : arr) {
+            if (num == 0) count1++;
+            if (num == 1) count2++;
+            if (num == 2) count3++;
+        }
+
+        for (int i = 0; i < count1; ++i) arr[i] = 0;
+        for (int i = 0; i < count2; ++i) arr[i] = 0;
+        for (int i = 0; i < count3; ++i) arr[i] = 0;
+    }
+
+    public void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void sortColors(int[] arr) {
+        int n = arr.length;
+        int l = 0;
+        int h = n - 1;
+        int i = 0;
+
+        while (i <= h) {
+            if (arr[i] == 0) {
+                swap(l, i, arr);
+                ++l;
+            } else if (arr[i] == 2) {
+                swap(i, h, arr);
+                --i;
+                --h;
             }
+
+            ++i;
         }
     }
 }

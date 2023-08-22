dffec3d99e9ed8fc9bc575320bb2858225e87b0f
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-08-22 12:44:16 +0530

    Solve 268

---
+++ src/_268_missing_number/Solution.java
@@ -0,0 +1,11 @@
+package _268_missing_number;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int i : nums) actualSum += i;
+        return expectedSum - actualSum;
+    }
+}

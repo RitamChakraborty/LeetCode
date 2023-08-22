db408b47e36130a09d688b957ca7afbdcc432ed4
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-08-22 10:16:38 +0530

    Add brute force solutions to 287

--- src/_287_find_the_duplicate_number/Solution.java
+++ src/_287_find_the_duplicate_number/Solution.java
@@ -1,6 +1,29 @@
 package _287_find_the_duplicate_number;
 
+import java.util.Arrays;
+
 class Solution {
+    public int findDuplicateBruteForce1(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] == nums[i - 1]) return nums[i];
+        }
+
+        return -1;
+    }
+
+    public int findDuplicateBruteForce2(int[] nums) {
+        int[] hash = new int[nums.length + 1];
+
+        for (int i : nums) {
+            if (hash[i] == 0) hash[i]++;
+            else return i;
+        }
+
+        return -1;
+    }
+
     public int findDuplicate(int[] nums) {
         int slow = 0;
         int fast = 0;

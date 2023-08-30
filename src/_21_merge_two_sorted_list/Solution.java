c846ca7e31ba91a800524665f108834b2120fb9b
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-08-30 17:54:38 +0530

    Solve 21

---
+++ src/_21_merge_two_sorted_list/Solution.java
@@ -0,0 +1,50 @@
+package _21_merge_two_sorted_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.val > list2.val) {
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+
+        ListNode result = list1;
+
+        while (list2 != null) {
+            ListNode tmp = null;
+
+            while (list1 != null && list1.val <= list2.val) {
+                tmp = list1;
+                list1 = list1.next;
+            }
+
+            tmp.next = list2;
+
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+
+        return result;
+    }
+}

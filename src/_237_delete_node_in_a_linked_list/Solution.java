095e6fb69ef6582b53306fdccf930dcf4d7300a0
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-08-31 07:21:16 +0530

    Solve 237

---
+++ src/_237_delete_node_in_a_linked_list/Solution.java
@@ -0,0 +1,17 @@
+package _237_delete_node_in_a_linked_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}

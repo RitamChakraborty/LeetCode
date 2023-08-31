fa2f173a113dec50c31d12e01510e588b7047b0b
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-08-31 08:22:55 +0530

    Solve 142

---
+++ src/_142_linked_list_cycle_ii/Solution.java
@@ -0,0 +1,37 @@
+package _142_linked_list_cycle_ii;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode entry = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                while (slow != entry) {
+                    slow = slow.next;
+                    entry = entry.next;
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}

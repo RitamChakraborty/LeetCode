ea98be4fd90b61edacf7ea715ac066454022ce54
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-09-25 14:09:20 +0530

    Solve 143

---
+++ src/_143_reorder_list/Solution.java
@@ -0,0 +1,59 @@
+package _143_reorder_list;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    private ListNode reverse(ListNode head) {
+        ListNode node = head;
+        ListNode prev = null;
+
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+
+        return prev;
+    }
+
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = reverse(slow.next);
+        slow.next = null;
+
+        ListNode l1 = head;
+        ListNode l2 = second;
+
+        while (l2 != null) {
+            ListNode l1Next = l1.next;
+            ListNode l2Next = l2.next;
+            l1.next = l2;
+            l2.next = l1Next;
+            l1 = l1Next;
+            l2 = l2Next;
+        }
+    }
+}

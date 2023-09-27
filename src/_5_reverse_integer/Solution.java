3bd5566c6632fa94f66a91e54fc1d280fb148edf
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-09-27 13:07:55 +0530

    Solve 5

---
+++ src/_5_reverse_integer/Solution.java
@@ -0,0 +1,21 @@
+package _5_reverse_integer;
+
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+
+       while (x != 0) {
+           int rem = x % 10;
+           x /= 10;
+
+           if (reversed < Integer.MIN_VALUE / 10 ||
+                   (reversed == Integer.MIN_VALUE / 10 && rem < Integer.MIN_VALUE % 10)) return 0;
+           if (reversed > Integer.MAX_VALUE / 10 ||
+                   (reversed == Integer.MAX_VALUE / 10 && rem > Integer.MAX_VALUE % 10)) return 0;
+
+           reversed = reversed * 10 + rem;
+       } 
+
+       return reversed;
+    }
+}

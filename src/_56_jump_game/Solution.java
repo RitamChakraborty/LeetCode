e9c63a436377f6b92a643f9c4e887f059188bedf
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-10-30 10:01:12 +0530

    Solve 56

---
+++ src/_56_jump_game/Solution.java
@@ -0,0 +1,15 @@
+package _56_jump_game;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int end = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; --i) {
+            if (i + nums[i] >= end) {
+                end = i;
+            }
+        }
+
+        return end == 0;
+    }
+}

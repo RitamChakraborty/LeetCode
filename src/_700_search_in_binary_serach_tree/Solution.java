e714fdad94eb8847ba3af051f729863a49fdf959
Ritam Chakraborty <ritam.chakraborty@cresensolutions.com>
2023-11-09 23:01:41 +0530

    Solve 700

---
+++ src/_700_search_in_binary_serach_tree/Solution.java
@@ -0,0 +1,29 @@
+package _700_search_in_binary_serach_tree;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) return root;
+        if (root.val < val) return searchBST(root.right, val);
+        return searchBST(root.left, val);
+    }
+}

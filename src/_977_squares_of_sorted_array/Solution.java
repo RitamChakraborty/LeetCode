6221d38e869dfc496dec5c1aeb4d347e106443cf
Ritam Chakraborty <postbox.ritamchakraborty@gmail.com>
2024-11-13 19:17:41 +0530

    Move 977

---
+++ src/_977_squares_of_sorted_array/Solution.java
@@ -0,0 +1,13 @@
+package _977_squares_of_sorted_array;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}

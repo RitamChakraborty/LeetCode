d393a2a6d16fe1494d0fa3c9292e88bfa23ef2aa
Ritam Chakraborty <postbox.ritamchakraborty@gmail.com>
2024-11-13 17:31:06 +0530

    Solve 94

---
+++ src/_94_binary_tree_inorder_traversal/Solution.java
@@ -0,0 +1,38 @@
+package _94_binary_tree_inorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        inorder(root, answer);
+        return answer;
+    }
+}

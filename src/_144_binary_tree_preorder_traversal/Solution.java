b620988e504b32c28016c1b6d8ee2cce0c552ec0
Ritam Chakraborty <postbox.ritamchakraborty@gmail.com>
2024-11-13 18:17:22 +0530

    MOve 144

---
+++ src/_144_binary_tree_preorder_traversal/Solution.java
@@ -0,0 +1,38 @@
+package _144_binary_tree_preorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private void preorder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        list.add(node.val);
+        preorder(node.left, list);
+        preorder(node.right, list);
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        preorder(root, answer);
+        return answer;
+    }
+}
